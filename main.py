2463e3da809587ef173c4a94aae97a29680494bc
100495924 <100495924@alumnos.uc3m.es>
2024-10-17 14:33:49 +0200

    Creados sistemas de Logueo y registro

--- main.py
+++ main.py
@@ -1,27 +1,90 @@
-class bank_user:
-    def __init__(self, doc_id, bank_num, nombre, apellidos, salt, pwd_hash, direccion, credito):
+import os
+import random
+
+class BankUser:
+    def __init__(self, doc_id, nombre, apellidos, pwd, direccion, credito):
         self.doc_id = doc_id
-        self.bank_num = bank_num
         self.nombre = nombre
         self.apellidos = apellidos
-        self.salt = salt
-        self.pwd_hash = pwd_hash
         self.direccion = direccion
         self.credito = credito
 
+        self.salt = os.urandom(16)
+        self.pwd_hash = hash(pwd)  # Habrá que usar un hash de la librería Scrypt.
+        generated_bank_num = str(random.randint(0, 9999999999999999999999))
+        # Verificar que el número del banco que se va a generar no existe.
+        self.bank_num = "ES" + generated_bank_num
 
-class Bank:
+
+class BankInstance:
+    def __init__(self):
+
+        self.users = []     # Todos los usuarios que el sistema maneja.
+
+    def search_user(self, user_doc_id):
+        for i in range(len(self.users)):
+            user = self.users[i]
+            if user.doc_id == user_doc_id:
+                return user
+            return None
+
+
+
+
     def register(self):
+        # Verificar que no hay ya un usuario con los mismos datos.
         print("Registrando\n"
               "Inserte sus datos:\n"
-              "Número de identidad: \n")
-        doc_id = input("")
+              "Número de identidad: ")
+        doc_id_user = input("")
+        print("Nombre: ")
+        nombre_user = input("")
+        print("Apellidos: ")
+        apellido_user = input("")
+        print("Dirección: ")
+        direccion_user = input("")
+        print("Inserta tu saldo inicial: ")
+        credito_user = int(input(""))
 
+        pwd_loop = True
+        while pwd_loop:
+            print("Escribe tu contraseña: ")
+            user_pwd = input("")
+            print("Repite la contraseña: ")
+            user_pwd_repeat = input("")
+            if user_pwd != user_pwd_repeat:
+                print("Las contraseñas no coinciden.")
+            else:
+                print("Usuario registrado con éxito.")
+                pwd_loop = False
 
+        bank_user = BankUser(doc_id_user, nombre_user, apellido_user, user_pwd,
+                             direccion_user, credito_user)
+        self.users.append(bank_user)
 
 
     def log_in(self):
-        print("Logueando")
+        user_to_login = None
+        print("Logueando\n"
+              "Introduce tus datos de inicio de sesión:\n")
+        doc_id_loop = True
+        while doc_id_loop:
+            login_doc_id = input("Documento de Identidad: \n")
+            user_to_login = self.search_user(login_doc_id)
+            if user_to_login is None:
+                print("¡Cuenta no encontrada!")
+            else:
+                doc_id_loop = False
+
+        pwd_loop = True
+        while pwd_loop:
+            login_pwd = input("Contraseña: \n")
+            if hash(login_pwd) == user_to_login.pwd_hash:   # Sustituir hash()
+                print("¡Bienvenido de vuelta!")
+                pwd_loop = False
+            else:
+                print("¡Contraseña incorrecta!")
+
 
     def bank_loop(self):
         account_loop = True
@@ -31,7 +94,7 @@ class Bank:
             print("¿Qué quieres hacer?\n"
                   "1_Registrar una cuenta\n"
                   "2_Entrar a una cuenta\n"
-                  "3_Salir\n")
+                  "3_Salir")
             user_input = input("")
 
             if user_input == "1":
@@ -42,14 +105,16 @@ class Bank:
                 account_loop = False
                 logued_in_loop = False
 
+        """
         while logued_in_loop:
             print("¿Qué quieres hacer con tu dinero?\n"
                   "Crédito: {}".format(credito))
-
+        """
         print("¡Hasta la próxima!")
 
 
-bank_loop()
+banko = BankInstance()
+banko.bank_loop()
 
 
 """
